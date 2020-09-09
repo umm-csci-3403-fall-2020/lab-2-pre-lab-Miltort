042fe2fae5c466df1fe14851ade1fa56e4c35e25
miltort <spilm005@umn.edu>
2020-09-09 17:54:38 -0500

    fix memory error in check_whitespace.c
    fixed memory leak that existed in check whitespace

--- check_whitespace.c
+++ check_whitespace.c
@@ -66,7 +66,7 @@ int is_clean(char* str) {
   // 0 if they're equal, and a positive value if the first is
   // greater than the second.
   result = strcmp(str, cleaned);
-
+  free(cleaned);
   return result == 0;
 }
 
@@ -90,6 +90,5 @@ int main() {
       printf("The string '%s' is NOT clean.\n", strings[i]);
     }
   }
-
   return 0;
 }
